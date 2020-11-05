b5c9f8ad00271e053505f573ddc202ccbc0d2d92
mohsinsk <sk08.mohsin@gmail.com>
2020-11-05 13:12:22 +0530

    Added Passbook

---
+++ app/src/main/java/com/mohsin/mvvmpractice2java/ui/passbook/PassbookActivity.java
@@ -0,0 +1,16 @@
+package com.mohsin.mvvmpractice2java.ui.passbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mohsin.mvvmpractice2java.R;
+
+public class PassbookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passbook);
+    }
+}
